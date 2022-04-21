d4db90f77556e40a0568cb365727f1242aedb3fc
Albert Olivé Corbella <albert.olive@letgo.com>
2022-04-21 17:46:28 +0200

    Using SWR

--- pages/api/getEvents.js
+++ pages/api/getEvents.js
@@ -4,9 +4,10 @@ export default async function handler(req, res) {
   const now = new Date();
   const from = now;
   const until = new Date(now.getFullYear(), now.getMonth() + 1);
+  const events = await getCalendarEvents(from, until);
 
   try {
-    res.status(200).json(await getCalendarEvents(from, until));
+    res.status(200).json(events);
   } catch (error) {
     console.error(error);
   }
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,18 @@ import React from "react";
 import Card from "@components/ui/card";
 import List from "@components/ui/list";
 import { server } from "../config";
+import useSWR from "swr";
 
-export default function App({ events = [] }) {
-  console.log(events);
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function App(props) {
+  const {
+    data: { events = [] },
+  } = useSWR("/api/getEvents", fetcher, {
+    fallbackData: props,
+    refreshInterval: 30000,
+  });
+
   return (
     <List events={events}>
       {(event) => <Card key={event.id} event={event} />}
@@ -12,14 +21,33 @@ export default function App({ events = [] }) {
   );
 }
 
-export const getStaticProps = async () => {
-  try {
-    const res = await fetch(`${server}/api/getEvents`);
-    const events = await res.json();
+export async function getStaticProps() {
+  const { getCalendarEvents } = require("@lib/helpers");
 
-    return { props: { events }, revalidate: 60 };
-  } catch (err) {
-    console.error(err);
-    return { props: { error: JSON.parse(JSON.stringify(err)) } };
-  }
-};
+  const now = new Date();
+  const from = now;
+  const until = new Date(now.getFullYear(), now.getMonth() + 1);
+
+  const { events } = await getCalendarEvents(from, until);
+  const normalizedEvents = JSON.parse(JSON.stringify(events));
+
+  return {
+    props: { events: normalizedEvents },
+    revalidate: 1,
+  };
+}
+
+// export const getStaticProps2 = async () => {
+//   try {
+//     const res = await fetch(`${server}/api/getEvents`);
+//     const events = await res.json();
+
+//     if (!res.ok)
+//       throw new Error(`Failed to fetch events, received status ${res.status}`);
+
+//     return { props: { events }, revalidate: 10 };
+//   } catch (err) {
+//     console.error(err);
+//     return { props: { error: JSON.parse(JSON.stringify(err)) } };
+//   }
+// };
