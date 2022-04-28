6a34c9ab429b35436d785e8417b64d6272dbd2ef
Albert Olivé Corbella <albert.olive@letgo.com>
2022-04-28 12:13:55 +0200

    Fixing esling problem

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,12 @@ function CulturaCardedeuMainEntry({ Component, pageProps }) {
   return (
     <>
       <Script
+        id="google-analytics-gtag"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script id="google-analytics-lazy-load" strategy="lazyOnload">
         {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
