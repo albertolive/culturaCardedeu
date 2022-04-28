7440944a155e8aa8b7f0495781faa0e999ac07eb
Albert Olivé Corbella <albert.olive@letgo.com>
2022-04-28 12:09:11 +0200

    Adding GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
 import "@styles/globals.css";
+import Script from "next/script";
 import { BaseLayout } from "@components/ui/layout";
 
 function CulturaCardedeuMainEntry({ Component, pageProps }) {
   return (
-    <BaseLayout>
-      <Component {...pageProps} />
-    </BaseLayout>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+
+      <BaseLayout>
+        <Component {...pageProps} />
+      </BaseLayout>
+    </>
   );
 }
 
