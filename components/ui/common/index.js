17783d5827daffeae52eac85aea4d7343d983d00
Albert Olivé Corbella <albert.olive@letgo.com>
2022-05-19 16:44:59 +0200

    Fixed typo

--- components/ui/common/index.js
+++ components/ui/common/index.js
@@ -3,4 +3,4 @@ export { default as Navbar } from "./navbar";
 export { default as ActiveLink } from "./link";
 export { default as Image } from "./image";
 export { default as Notification } from "./notification";
-export { default as SubMenu } from "./submenu";
+export { default as SubMenu } from "./subMenu";
