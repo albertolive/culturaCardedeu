3e9a55817bc9fe609ec6ec091d57616655bbda5d
Albert Olivé Corbella <albert.olive@letgo.com>
2022-06-05 14:46:00 +0200

    Fixed footer

--- components/ui/common/footer/index.js
+++ components/ui/common/footer/index.js
@@ -3,11 +3,36 @@ export default function Footer() {
     <footer className="bg-gray-900 ">
       <div className="container mx-auto px-6">
         <div className="mt-5 flex flex-col items-center">
-          <div className="py-6">
-            <p className="mb-6 text-white text-sm text-primary-2 font-bold">
+          <div className="py-4">
+            <div className="text-white text-sm text-primary-2 font-bold text-center">
               © {new Date().getFullYear()} Cultura Cardedeu <br />
-              <a href="hola@culturacardedeu.com">hola@culturacardedeu.com</a>
-            </p>
+              <a
+                className="hover:text-[#ECB84A]"
+                href="mailto:hola@culturacardedeu.com"
+              >
+                hola@culturacardedeu.com
+              </a>
+              <div className="flex space-x-2">
+                <a
+                  className="hover:text-[#ECB84A]"
+                  href="https://twitter.com/culturacardedeu"
+                >
+                  Twitter
+                </a>
+                <a
+                  className="hover:text-[#ECB84A]"
+                  href="https://t.me/culturacardedeu"
+                >
+                  Telegram
+                </a>
+                <a
+                  className="hover:text-[#ECB84A]"
+                  href="https://www.facebook.com/culturacardedeu"
+                >
+                  Facebook
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
