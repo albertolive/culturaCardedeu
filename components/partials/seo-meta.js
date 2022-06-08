da12c77db6de7879917384e7e8ab204bec618cae
Albert Olivé Corbella <albert.olive@letgo.com>
2022-06-08 10:51:55 +0200

    Fixed httpEquiv

--- components/partials/seo-meta.js
+++ components/partials/seo-meta.js
@@ -13,8 +13,8 @@ const Meta = (props) => {
     <Head>
       <title>{props.title}</title>
       <meta charset="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-      <meta http-equiv="Content-Language" content="ca" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta httpEquiv="Content-Language" content="ca" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
         name="robots"
