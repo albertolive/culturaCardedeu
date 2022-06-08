c559959bb54faa59a2cf8b7cc99cb033086d5c3b
Albert Olivé Corbella <albert.olive@letgo.com>
2022-06-08 10:50:24 +0200

    Fixed meta attributes

--- components/partials/seo-meta.js
+++ components/partials/seo-meta.js
@@ -12,7 +12,9 @@ const Meta = (props) => {
   return (
     <Head>
       <title>{props.title}</title>
-      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta http-equiv="Content-Language" content="ca" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
         name="robots"
