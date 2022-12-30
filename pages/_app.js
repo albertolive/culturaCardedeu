ea56937a87e247e72ac38fd08600967a1c01805b
Albert Olive <albertolivecorbella@gmail.com>
2022-12-30 20:25:42 +0100

    test

--- pages/_app.js
+++ pages/_app.js
@@ -51,12 +51,6 @@ function CulturaCardedeuMainEntry({ Component, pageProps }) {
         });
     `}
       </Script>
-      {/* <Script
-        id="google-ads"
-        strategy="lazyOnload"
-        crossOrigin="anonymous"
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS}`}
-      /> */}
       <BaseLayout>
         <Component {...pageProps} />
       </BaseLayout>
