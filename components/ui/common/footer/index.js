4ba5c5d77c2ca99c49d31251eb4ae3e2528f6d75
Albert Olive <albertolivecorbella@gmail.com>
2023-01-08 14:22:12 +0100

    Fixed footer issues

--- components/ui/common/footer/index.js
+++ components/ui/common/footer/index.js
@@ -1,52 +1,50 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 h-[92px]">
-      <div className="container mx-auto px-6">
-        <div className="mt-5 flex flex-col items-center">
-          <div className="py-4">
-            <div className="text-white text-sm text-primary-2 font-bold text-center">
-              © {new Date().getFullYear()} Cultura Cardedeu <br />
-              Contacte:&nbsp;
-              <a
-                className="hover:text-[#ECB84A]"
-                href="mailto:hola@culturacardedeu.com"
-              >
-                hola@culturacardedeu.com
-              </a>
-              <div className="flex space-x-2">
-                Segueix-nos a:&nbsp;
-                <div>
-                  <a
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className="hover:text-[#ECB84A]"
-                    href="https://twitter.com/culturacardedeu"
-                  >
-                    Twitter
-                  </a>
-                </div>
-                <div>
-                  |&nbsp;&nbsp;
-                  <a
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className="hover:text-[#ECB84A]"
-                    href="https://t.me/culturacardedeu"
-                  >
-                    Telegram
-                  </a>
-                </div>
-                <div>
-                  |&nbsp;&nbsp;
-                  <a
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className="hover:text-[#ECB84A]"
-                    href="https://www.facebook.com/culturacardedeu"
-                  >
-                    Facebook
-                  </a>
-                </div>
+    <footer className="bg-gray-900 h-[85px] absolute bottom-0 w-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col justify-center items-center h-full">
+          <div className="text-white text-sm text-primary-2 font-bold text-center">
+            © {new Date().getFullYear()} Cultura Cardedeu <br />
+            Contacte:&nbsp;
+            <a
+              className="hover:text-[#ECB84A]"
+              href="mailto:hola@culturacardedeu.com"
+            >
+              hola@culturacardedeu.com
+            </a>
+            <div className="flex space-x-2">
+              Segueix-nos a:&nbsp;
+              <div>
+                <a
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="hover:text-[#ECB84A]"
+                  href="https://twitter.com/culturacardedeu"
+                >
+                  Twitter
+                </a>
+              </div>
+              <div>
+                |&nbsp;&nbsp;
+                <a
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="hover:text-[#ECB84A]"
+                  href="https://t.me/culturacardedeu"
+                >
+                  Telegram
+                </a>
+              </div>
+              <div>
+                |&nbsp;&nbsp;
+                <a
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="hover:text-[#ECB84A]"
+                  href="https://www.facebook.com/culturacardedeu"
+                >
+                  Facebook
+                </a>
               </div>
             </div>
           </div>
--- components/ui/layout/base/index.js
+++ components/ui/layout/base/index.js
@@ -18,8 +18,8 @@ export default function BaseLayout({ children }) {
       </Head>
       <Navbar />
       {/* <Notify /> */}
-      <div className="mx-auto">
-        <div className="max-w-7xl mx-auto fit p-4 xl:p-0 xl:py-4">
+      <div className="mx-auto pb-[85px]">
+        <div className="fit max-w-7xl mx-auto p-4 xl:p-0 xl:py-4">
           {children}
         </div>
       </div>
