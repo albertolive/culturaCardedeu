3672775956b9a737ad52bbec59a78a591e54edbf
Albert Olivé <albertolivecorbella@gmail.com>
2023-04-08 13:58:19 +0200

    Improved textarea

--- components/ui/common/form/textarea/index.js
+++ components/ui/common/form/textarea/index.js
@@ -2,6 +2,26 @@ import dynamic from "next/dynamic";
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import "react-quill/dist/quill.snow.css";
 
+const modules = {
+  toolbar: [
+    [{ 'header': [1, 2, false] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+    ['link', 'image'],
+    ['clean']
+  ],
+  clipboard: {
+    matchVisual: false
+  }
+}
+
+const formats = [
+  'header',
+  'bold', 'italic', 'underline', 'strike', 'blockquote',
+  'list', 'bullet', 'indent',
+  'link', 'image'
+]
+
 export default function TextArea({ id, value = "", onChange }) {
   const onChangeContent = (value) => onChange({ target: { name: id, value } });
 
@@ -19,6 +39,9 @@ export default function TextArea({ id, value = "", onChange }) {
           theme="snow"
           defaultValue={value}
           onChange={onChangeContent}
+          preserveWhitespace
+          modules={modules}
+          formats={formats}
           className="shadow-sm block w-full sm:text-sm border-gray-300 rounded-md"
         />
       </div>
