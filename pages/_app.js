f5c149a081409bf4e56551421da0edf4481b8db6
Albert Olivé <albertolivecorbella@gmail.com>
2023-07-13 07:47:17 +0200

    Removed buymeacoffee

--- pages/_app.js
+++ pages/_app.js
@@ -5,40 +5,6 @@ import Script from "next/script";
 import { BaseLayout } from "@components/ui/layout";
 import { useRouter } from "next/router";
 
-function Buymeacoffee() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    const div = document.getElementById("supportByBMC");
-    script.setAttribute(
-      "src",
-      "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-    );
-    script.setAttribute("data-name", "BMC-Widget");
-    script.setAttribute("data-cfasync", "false");
-    script.setAttribute("data-id", "CulturaCardedeu");
-    script.setAttribute("data-description", "Regala'm cultura :)");
-    script.setAttribute(
-      "data-message",
-      "Cultura Cardedeu és un projecte participatiu per descobrir de manera ràpida i senzilla tots els esdeveniments culturals que tenen lloc a Cardedeu. Amb la vostra col·laboració, ens ajudeu a mantenir els servidors que fan possible aquesta iniciativa!"
-    );
-    script.setAttribute("data-color", "#ECB84A");
-    script.setAttribute("data-position", "Right");
-    script.setAttribute("data-x_margin", "18");
-    script.setAttribute("data-y_margin", "18");
-
-    script.onload = function () {
-      var evt = document.createEvent("Event");
-      evt.initEvent("DOMContentLoaded", false, false);
-      window.dispatchEvent(evt);
-    };
-
-    div.appendChild(script);
-  }, []);
-
-  return <div id="supportByBMC"></div>;
-}
-
-
 function CulturaCardedeuMainEntry({ Component, pageProps }) {
   const { events } = useRouter();
   const infolinks_pid = 3386219;
@@ -94,7 +60,6 @@ function CulturaCardedeuMainEntry({ Component, pageProps }) {
 
       <BaseLayout>
         <Component {...pageProps} />
-        <Buymeacoffee />
       </BaseLayout>
     </>
   );
