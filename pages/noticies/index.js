af30809b65569b364731e96898b854129e9749ee
Albert Olivé <albertolivecorbella@gmail.com>
2025-06-04 09:52:32 +0200

    wip

--- pages/noticies/index.js
+++ pages/noticies/index.js
@@ -387,7 +387,7 @@ export default function NoticiesPage({ newsSummaries, hasError }) {
                 <div className="flex justify-center space-x-4">
                   <Link href="/avui-a-cardedeu">
                     <a className="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
-                      Veure esdeveniments d&apos;avui
+                      Avui a Cardedeu
                     </a>
                   </Link>
                   <Link href="/setmana-a-cardedeu">
@@ -412,7 +412,7 @@ export default function NoticiesPage({ newsSummaries, hasError }) {
               <div className="flex justify-center space-x-4">
                 <Link href="/avui-a-cardedeu">
                   <a className="inline-flex items-center px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
-                    Veure esdeveniments d&apos;avui
+                    Avui a Cardedeu
                   </a>
                 </Link>
                 <Link href="/setmana-a-cardedeu">
